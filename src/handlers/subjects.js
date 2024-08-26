be031996e28038de4bd0fb1dd953f6ad47120d93
Noga geller <nogag098@gmail.com>
2024-08-26 15:29:42 +0300

    remove try and catch handlers

--- src/handlers/subjects.js
+++ src/handlers/subjects.js
@@ -3,63 +3,46 @@ const Joi = require('joi');
 const { StatusCodes } = require('http-status-codes');
 
 const createSubject = async (req, res) => {
-    try {
-        const { body } = req;
-        const result = await req.collectionOperations.insertOne(body);
-        return res.status(StatusCodes.CREATED).json(result);
-    } catch (error) {
-        console.error('Error adding subject:', error);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error adding subject', error });
-    }
+    const { body } = req;
+    const result = await req.collectionOperations.insertOne(body);
+    return res.status(StatusCodes.CREATED).json(result);
 };
 
 const deleteSubject = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const objectId = new ObjectId(id);
-        const result = await req.collectionOperations.deleteOne({ _id: objectId });
+    const { id } = req.params;
+    const objectId = new ObjectId(id);
+    const result = await req.collectionOperations.deleteOne({ _id: objectId });
 
-        if (result.deletedCount === 0) {
-            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Subject not found' });
-        }
-
-        return res.status(StatusCodes.OK).json(result);
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error deleting subject', error });
+    if (result.deletedCount === 0) {
+        return res.status(StatusCodes.NOT_FOUND).json({ message: 'Subject not found' });
     }
+
+    return res.status(StatusCodes.OK).json(result);
 };
 
 const getSubjects = async (req, res) => {
-    try {
-        const subjects = await req.collectionOperations.find({});
-        return res.status(StatusCodes.OK).json(subjects);
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error getting subjects', error });
-    }
+    const subjects = await req.collectionOperations.find({});
+    return res.status(StatusCodes.OK).json(subjects);
 };
 
 const updateSubject = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const objectId = new ObjectId(id);
-        const { name } = req.body;
+    const { id } = req.params;
+    const objectId = new ObjectId(id);
+    const { name } = req.body;
 
-        const updateFields = {};
-        if (name !== undefined) updateFields.name = name;
+    const updateFields = {};
+    if (name !== undefined) updateFields.name = name;
 
-        const result = await req.collectionOperations.updateOne(
-            { _id: objectId },
-            { $set: updateFields }
-        );
+    const result = await req.collectionOperations.updateOne(
+        { _id: objectId },
+        { $set: updateFields }
+    );
 
-        if (result.matchedCount === 0) {
-            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Subject not found' });
-        }
-
-        return res.status(StatusCodes.OK).json(result);
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error updating subject', error });
+    if (result.matchedCount === 0) {
+        return res.status(StatusCodes.NOT_FOUND).json({ message: 'Subject not found' });
     }
+
+    return res.status(StatusCodes.OK).json(result);
 };
 
 module.exports = { createSubject, deleteSubject, getSubjects, updateSubject };
--- src/handlers/tasks.js
+++ src/handlers/tasks.js
@@ -3,69 +3,49 @@ const Joi = require('joi');
 const { StatusCodes } = require('http-status-codes');
 
 const createTask = async (req, res) => {
-    try {
-        const { body } = req;
-        const result = await req.collectionOperations.insertOne(body);
-        return res.status(StatusCodes.CREATED).json(result);
-    } catch (error) {
-        console.error('Error adding task:', error);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error adding task', error });
-    }
+    const { body } = req;
+    const result = await req.collectionOperations.insertOne(body);
+    return res.status(StatusCodes.CREATED).json(result);
 };
 
 const deleteTask = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const objectId = new ObjectId(id);
-        const result = await req.collectionOperations.deleteOne({ _id: objectId });
+    const { id } = req.params;
+    const objectId = new ObjectId(id);
+    const result = await req.collectionOperations.deleteOne({ _id: objectId });
 
-        if (result.deletedCount === 0) {
-            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Task not found' });
-        }
-
-        return res.status(StatusCodes.OK).json(result);
-    } catch (error) {
-        console.error('Error deleting task:', error);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error deleting task', error });
+    if (result.deletedCount === 0) {
+        return res.status(StatusCodes.NOT_FOUND).json({ message: 'Task not found' });
     }
+
+    return res.status(StatusCodes.OK).json(result);
 };
 
 const getTasks = async (req, res) => {
-    try {
-        const tasks = await req.collectionOperations.find({});
-        return res.status(StatusCodes.OK).json(tasks);
-    } catch (error) {
-        console.error('Error getting tasks:', error);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error getting tasks', error });
-    }
+    const tasks = await req.collectionOperations.find({});
+    return res.status(StatusCodes.OK).json(tasks);
 };
 
 const updateTask = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const objectId = new ObjectId(id);
-        const { name, subject, priority, completed } = req.body;
+    const { id } = req.params;
+    const objectId = new ObjectId(id);
+    const { name, subject, priority, completed } = req.body;
 
-        const updateFields = {};
-        if (name !== undefined) updateFields.name = name;
-        if (subject !== undefined) updateFields.subject = subject;
-        if (priority !== undefined) updateFields.priority = priority;
-        if (completed !== undefined) updateFields.completed = completed;
+    const updateFields = {};
+    if (name !== undefined) updateFields.name = name;
+    if (subject !== undefined) updateFields.subject = subject;
+    if (priority !== undefined) updateFields.priority = priority;
+    if (completed !== undefined) updateFields.completed = completed;
 
-        const result = await req.collectionOperations.updateOne(
-            { _id: objectId },
-            { $set: updateFields }
-        );
+    const result = await req.collectionOperations.updateOne(
+        { _id: objectId },
+        { $set: updateFields }
+    );
 
-        if (result.matchedCount === 0) {
-            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Task not found' });
-        }
-
-        return res.status(StatusCodes.OK).json(result);
-    } catch (error) {
-        console.error('Error updating task:', error);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error updating task', error });
+    if (result.matchedCount === 0) {
+        return res.status(StatusCodes.NOT_FOUND).json({ message: 'Task not found' });
     }
+
+    return res.status(StatusCodes.OK).json(result);
 };
 
 module.exports = { createTask, deleteTask, getTasks, updateTask };
